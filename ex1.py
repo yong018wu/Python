71937326507c503f00620eef6d6da372e927d290
yong018wu <yong018wu@gmail.com>
2016-09-23 01:13:49 -0700

    commit

---
+++ ex1.py
@@ -0,0 +1,8 @@
+__author__='Craig Richard'
+__version__ = '1.0'
+
+import os
+import sys
+
+
+if __name__ '__main__'
---
+++ test.py
@@ -0,0 +1,5 @@
+f = open("test.txt",'r')
+for line in f:
+    line = line.decode('utf-8')
+    print line
+
